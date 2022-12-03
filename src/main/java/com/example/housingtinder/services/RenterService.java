470494910d506cae668c5ca9502c89e511515d25
Vasyl Paliuha <vasyl.paliuha@student.tuke.sk>
2022-12-03 18:01:42 +0100

    RenterService

--- src/main/java/com/example/housingtinder/services/RenterService.java
+++ src/main/java/com/example/housingtinder/services/RenterService.java
@@ -1,4 +1,21 @@
 package com.example.housingtinder.services;
 
+import com.example.housingtinder.entyties.renter.Renter;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.util.List;
+
 public class RenterService {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void addRenter(Renter renter){
+        entityManager.persist(renter);
+    }
+
+    public List<Renter> getRenters(){
+        return entityManager.createQuery("select r from Renter r").getResultList();
+    }
 }
