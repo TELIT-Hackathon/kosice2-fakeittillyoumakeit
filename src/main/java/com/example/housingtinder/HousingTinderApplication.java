dab8b2ed94ac0460ccb3ca1a9821a756131355bb
parz1val <yurii.levhenko@student.tuke.sk>
2022-12-03 13:06:39 +0100

    Init

---
+++ src/main/java/com/example/housingtinder/HousingTinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.housingtinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingTinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousingTinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/housingtinder/HousingTinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.housingtinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingTinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
